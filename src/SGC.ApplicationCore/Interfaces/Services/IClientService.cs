13372ee5cfbaf6a166fef56385ce9e8f9489c240
Rahul Sen <rsen253@hotmail.com>
2018-08-19 21:09:55 +0530

    Added Service demo interfaces and classes

---
+++ src/SGC.ApplicationCore/Interfaces/Services/IClientService.cs
@@ -0,0 +1,17 @@
+﻿using SGC.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SGC.ApplicationCore.Interfaces.Services
+{
+    public interface IClientService
+    {
+        Client Add(Client entity);
+        void Update(Client entity);
+        IEnumerable<Client> GetTodos();
+        Client GetById(int id);
+        IEnumerable<Client> Search(Expression<Func<Client, bool>> Predicate);
+        void Remove(Client entity);
+    }
+}
---
+++ src/SGC.ApplicationCore/Services/ClientService.cs
@@ -0,0 +1,49 @@
+﻿using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.ApplicationCore.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SGC.ApplicationCore.Services
+{
+    public class ClientService : IClientService
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientService(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public Client Add(Client entity)
+        {
+            return _clientRepository.Add(entity);
+        }
+
+        public Client GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> GetTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client> Search(Expression<Func<Client, bool>> Predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Client entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
